087472b079828b4b360c98b1c5bf695ccc621170
dominici94 <mattiadominici94@gmail.com>
2021-11-18 18:34:06 +0100

    risultati vittoria/sconfitta

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,10 @@
-// Consegna
-// Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
-// ****L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range: con difficoltà 1 => tra 1 e 100 con difficoltà 2 => tra 1 e 81 con difficoltà 3 => tra 1 e 49
-// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. I numeri nella lista delle bombe non possono essere duplicati.
+
 // In seguito l'utente clicca su ogni cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle. La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti. Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 // BONUS: Quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste.
 // Consigli del giorno: :party_wizard: ****Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi. Ad esempio: Di cosa ho bisogno per generare i numeri? Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti. Le validazioni e i controlli possiamo farli anche in un secondo momento.
 
-document.getElementById('play').addEventListener('click', function(){
+const gioca = document.getElementById('play');
+gioca.addEventListener('click', function(){
     play();
 });
 
@@ -31,6 +29,7 @@ function play(){
     }
 
     const bombe = generaBombe();
+    console.log(bombe);
 
     function generaBombe(){
         const arrayBombe = [];
@@ -55,17 +54,26 @@ function play(){
 
     function generaCampo(){
         const container = document.querySelector('.container-square');
-
+        let count = 0;
         for(let i=1; i<= boxNumero; i++){
             const box = creaElemento(i); 
 
             box.addEventListener('click', function(){
-                this.classList.add('color');
+                if(bombe.includes(i)){
+                    this.classList.add('bomba');
+                    return alert("Hai calpestato una bomba e hai perso. clicca 'inizia' per giocare ancora. "+ `Hai totalizzato ${count} punti.`), play();
+                }else if(!bombe.includes(i)){
+                    this.classList.add('safe');
+                    count++;
+                    console.log(count);
+                }
+                if(count == (boxNumero - bombeNumero)){
+                    return alert("Hai vinto!"), play();
+                }
             });
-
+            
             container.appendChild(box);
         }
-
     }
 
     function creaElemento(num){
@@ -79,5 +87,7 @@ function play(){
         return cella;
 
     }
+
+
 
 }
